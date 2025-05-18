817aab6776e973c65896d3269fb06fbe7b9e7794
hatopoppo0905 <hosikawadaisuki0611@gmail.com>
2025-05-18 09:05:05 +0000

    5月18日

---
+++ dentaku.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+float tasu(float a,float b);
+float hiku(float a,float b);
+float kake(float a,float b);
+
+int main(void){
+
+    float a,b;
+    int n;
+
+    printf("モードを選択してください（1+.2-.3*）\n");
+    scanf("%d\n",&n);
+    
+    return 0;
+}
